ce645646162a53044e116b0b9b1dd37d9d9d5eaa
kimdongheecom <dongheekim@gmail.com>
2025-02-05 18:29:43 +0900

    선형자료구조 250205

--- app.py
+++ app.py
@@ -4,8 +4,9 @@ app = Flask(__name__)
 @app.route('/', methods=['POST', 'GET'])
 def index():
     print("전송된 데이터 방식")
-
-    coin_500 = coin_100 = coin_50 = coin_5 = 0
+    
+    #초기값으로 설정
+    won_500 = won_100 = won_50 = won_5 = 0
     total = None
     price = None
     amount = None
@@ -17,7 +18,7 @@ def index():
 
         total = request.form.get('total')
         price = request.form.get('price')
-
+        #문자를 상수로 변환하기 위해 int를 붙임
         total = int(total)
         price = int(price)
 
@@ -28,25 +29,60 @@ def index():
             amount = total - price
         print(f"거스름돈: {amount}")
 
-        COIN_500 = 500
-        COIN_100 = 100
-        COIN_50 = 50
-        COIN_10 = 10
+        WON_50000 = 50000
+        WON_10000 = 10000
+        WON_5000 = 5000
+        WON_1000 = 1000 
+        WON_500 = 500
+        WON_100 = 100
+        WON_50 = 50
+        WON_10 = 10
+
+        won_list = [WON_50000,WON_10000,WON_5000,WON_1000, WON_500, WON_100, WON_50, WON_10]
+        # money_list = [50000, 10000, 5000, 1000, 500, 100, 50, 10]
+        # 50000 = 0번 index, 10000 = 1번 index,,,,,즉, money_list[1] =10000
+       
+        won50000 = amount // won_list[0]   
+        won50000_nmg = amount % won_list[0]
+
+        won10000 = won50000_nmg // won_list[1]   
+        won10000_nmg = won50000_nmg % won_list[1]
+
+        won5000 = won10000_nmg // won_list[2]   
+        won5000_nmg = won10000_nmg % won_list[2]
+
+        won1000 = won5000_nmg // won_list[3]   
+        won1000_nmg = won5000_nmg % won_list[3]
         
-        coin500 = amount // COIN_500   
-        coin500_nmg = amount % COIN_500
+        won500 = won1000_nmg // won_list[4]   
+        won500_nmg = won1000_nmg % won_list[4]
 
-        coin100 = coin500_nmg // COIN_100
-        coin100_nmg = coin500_nmg % COIN_100
+        won100 = won500_nmg // won_list[5]
+        won100_nmg = won500_nmg % won_list[5]
 
-        coin50 = coin100_nmg // COIN_50
-        coin50_nmg = coin100_nmg % COIN_50
+        won50 = won100_nmg // won_list[6]
+        won50_nmg = won100_nmg % won_list[6]
 
-        coin10 = coin50_nmg // COIN_10
-        coin10_nmg = coin50_nmg % COIN_10
+        won10 = won50_nmg // won_list[7]
+        won10_nmg = won50_nmg % won_list[7]
 
+        print(won_list[0])
+        print(won_list[1])
+        print(won_list[2])
+        print(won_list[3])
+        print(won_list[4])
+        print(won_list[5])
+        print(won_list[6])
+        print(won_list[7])
+        
+        #위에 프린터들은 아래 for구문 won_list
 
-        return render_template("index.html", coin500 = coin500, coin100 = coin100, coin50 = coin50, coin10 = coin10, amount = amount)  
+        for won in won_list:
+            print(won)
+
+        return render_template("index.html", won50000 = won50000, won10000 = won10000, won5000 = won5000, 
+                               won1000 = won1000, won500 = won500, won100 = won100, won50 = won50, 
+                               won10 = won10, amount = amount)  
     else:
         print("😙get 방식으로 진입")
 
